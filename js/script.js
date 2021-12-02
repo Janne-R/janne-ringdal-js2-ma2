ba72d94a81157a86967f89cfb3ca2ed5cef9ee23
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-12-02 15:56:28 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let listItems = [];
+import { saveToStorage, getFromStorage } from "./components/localStorage.js";
+import { listKey } from "./constants/settings.js";
 
 const listInput = document.querySelector("input");
 const button = document.querySelector("button");
@@ -18,6 +19,22 @@ const createList = () => {
     `;
   });
 
+  const removeFromList = (event) => {
+
+    const deleteItem = event.target.dataset.item;
+    console.log(deleteItem);
+
+    const newList = listItems.filter((item) => {
+      if (deleteItem !== item) {
+        return true;
+      }
+
+    });
+    listItems = newList;
+    createList();
+    saveToStorage(listKey, listItems);
+  }
+
   const trashCans = document.querySelectorAll("li i");
 
   trashCans.forEach((can) => {
@@ -25,12 +42,17 @@ const createList = () => {
   });
 }
 
+let listItems = getFromStorage(listKey);
+createList(listItems);
+
+
 const addToList = () => {
   const newBook = listInput.value.trim();
 
   if (newBook.length >= 1) {
     listItems.push(newBook);
     createList();
+    saveToStorage(listKey, listItems);
     listInput.value = "";
     listInput.focus();
   }
@@ -38,20 +60,7 @@ const addToList = () => {
 
 button.addEventListener("click", addToList);
 
-const removeFromList = (event) => {
 
-  const deleteItem = event.target.dataset.item;
-  console.log(deleteItem);
-
-  const newList = listItems.filter((item) => {
-    if (deleteItem !== item) {
-      return true;
-    }
-
-  });
-  listItems = newList;
-  createList();
-}
 
 
 
